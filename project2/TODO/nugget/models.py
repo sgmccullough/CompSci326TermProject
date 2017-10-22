37d78a7b2805cf25e61fdfaecf8967b9c1bef230
Emily Goroza <admin@1x-nat-vl931-172-30-190-202.wireless.umass.edu>
2017-10-22 16:07:27 -0400

    Added User model.

--- project2/TODO/nugget/models.py
+++ project2/TODO/nugget/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """
+    Model representing a User Account.
+    """
+    #Fields
+    nug_id = models.IntegerField(help_text="Nugget ID unique to User")
+    usr = models.CharField(max_length=25, help_text="Username")
+    email = models.EmailField(max_length=50, help_text="User Email")
+    pswd = models.CharField(max_length=50, help_text="User Password")
+    bday = models.DateField(auto_now=True)
+    coins = models.IntegerField(help_text="User Currency")
+    #Messages & Settings should be their own models ?
+
+    #Metadeta
+    class Meta:
+        ordering = ["nug_id", "usr", "email", "pswd", "bday", "coins"]
+
+    #Methods
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of User
+        """
+        return reverse('user-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the User object (in Admin site)
+        """
+        return '%s (%s)' % (self.usr, self.nug_id)
