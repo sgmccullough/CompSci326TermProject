78f3922ea40d7ec53d6fe2f8233f678c94d87ad3
Emily Goroza <admin@1x-nat-vl931-172-30-159-35.wireless.umass.edu>
2017-10-25 19:13:31 -0400

    Added Views.

--- project2/TODO/nugget/views.py
+++ project2/TODO/nugget/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import User, Nugget, NuggetAttribute, Inventorie, Shop, Item, battle, nug_ids
+
+def index(request):
+    """
+    View function for login page.
+    """
+    return render(
+        request,
+        'index.html',
+    )
+
+def home(request):
+    """
+    View function for home page.
+    """
+    return render(
+        request,
+        'home.html',
+    )
+
+def nugget(request):
+    """
+    View function for nugget page.
+    """
+    return render(
+        request,
+        'nugget.html',
+    )
+
+def shop(request):
+    """
+    View function for shop page.
+    """
+    return render(
+        request,
+        'shop.html',
+    )
+
+def chat(request):
+    """
+    View function for chat page.
+    """
+    return render(
+        request,
+        'chat.html',
+    )
+
+def battle(request):
+    """
+    View function for battle page.
+    """
+    return render(
+        request,
+        'battle.html',
+    )
+
+def create(request):
+    """
+    View function for create page.
+    """
+    return render(
+        request,
+        'create-a-nugget.html',
+    )
